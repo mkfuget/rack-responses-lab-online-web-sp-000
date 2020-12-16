79a4458b26d414eba09315f160586b8db89dd01b
Michael Fuget <michaelfuget@gmail.com>
2020-12-16 03:45:33 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,14 @@
 class Application
   def call(env)
     resp = Rack::Response.new
-    current_time = Datetime.now
+    current_hour = Time.now.hour
+    if(current_hour<12)
+      resp.write "Good Morning!"
+    else 
+      resp.write "Good Morning!"
+    end
+
+      
     resp.finish
   end
 
